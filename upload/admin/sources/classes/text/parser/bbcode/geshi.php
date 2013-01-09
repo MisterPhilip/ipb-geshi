c3da0b352246bd68ac830d72b4e50ad6f89d15de
Philip Lawrence <philip@misterphilip.com>
2013-01-08 22:19:58 -0600

    Optimizing constructor / code box finding methods to match ipb-syntaxhighlighter

--- upload/admin/sources/classes/text/parser/bbcode/geshi.php
+++ upload/admin/sources/classes/text/parser/bbcode/geshi.php
@@ -102,84 +102,139 @@ class bbcode_plugin_geshi extends bbcode_plugin_code
     protected $_overallCssClass = 'foobar';
     
     
-	/**
-	 * {@inherit}
-	 */
-	public function __construct( ipsRegistry $registry, $_parent='' )
-	{
-		$this->currentBbcode	= 'geshi';
-        $this->_parent = $_parent;
-		
-		parent::__construct( $registry, $_parent );
-	}
-	
-	/**
-	 * Do the actual replacement
-	 *
-	 * @access	protected
-	 * @param	string		$txt	Parsed text from database to be edited
-	 * @return	string				BBCode content, ready for editing
-	 */
-	protected function _replaceText( $txt )
-	{
+    /**
+     * {@inherit}
+     */
+    public function __construct( ipsRegistry $registry, $_parent=NULL )
+    {
+        $this->currentBbcode    = 'geshi';
+        $this->_parent          = $_parent;
+        
+        // Do what is normally done in the parent __construct
+        // We can't call parent::__construct since bbcode_plugin_code would overwrite the currentBbcode
+        $this->registry        =  $registry;
+        $this->DB              =  $this->registry->DB();
+        $this->settings        =& $this->registry->fetchSettings();
+        $this->request         =& $this->registry->fetchRequest();
+        $this->lang            =  $this->registry->getClass('class_localization');
+        $this->member          =  $this->registry->member();
+        $this->memberData      =& $this->registry->member()->fetchMemberData();
+        $this->cache           =  $this->registry->cache();
+        $this->caches          =& $this->registry->cache()->fetchCaches();
+        
+        $this->_parentBBcode = $_parent;
+        
+        /* Retrieve bbcode data */
+        $bbcodeCache    = $this->cache->getCache('bbcode');
+        $this->_bbcode  = $bbcodeCache[ $this->currentBbcode ];
+    }
+    
+    /**
+     * Do the actual replacement
+     *
+     * @access    protected
+     * @param     string        $txt    Parsed text from database to be edited
+     * @return    string                BBCode content, ready for editing
+     */
+    protected function _replaceText( $txt )
+    {
+        // Convert old tag (and outdated tags) to the new code tag
+        $oldTags = array( );
+        
+        if( $this->settings['codesyntax_parseOld'] )
+            $oldTags = array_merge( $oldTags, array( 'html', 'php', 'sql', 'xml' ) );
+        
+        foreach( $this->_retrieveTags() as $tag)
+        {
+            if( in_array( $tag, $oldTags ) )
+            {
+                $lang = $tag;
+            }
+            else
+            {
+                $lang = 'auto';
+            }
+            
+            if ( stristr( $txt, '[' . $tag . ']' ) )
+            {
+                $txt = str_ireplace( '[' . $tag . ']', '[code=' . $lang . ':0]', $txt );
+            } 
+                
+            if ( $tag != 'code' && stristr( $txt, '[/' . $tag . ']' ) )
+            {
+                $txt = str_ireplace( '[/' . $tag . ']', '[/code]', $txt );
+            }
+        }
         // Check for the correct class, less resource intensive than regex
         if( stristr( $txt, '_prettyXPrint' ) )
         {
-            // We already have the HTML for it, let's clean it up and GeSHi it
-            if( preg_match_all( '#<pre[^>]*class\s?=\s?(["\'])((?:(?!\1).)*)_prettyXprint((?:(?!\1).)*)\1[^>]*>((?:(?!</pre>).)*)</pre>#is', $txt, $matches ) > 0 )
+            $tags = $this->_parent->getTagPositions( $txt, 'pre', array( '<' , '>' ) );
+            
+            foreach( $tags['open'] as $id => $val )
             {
-                $codeboxCount = count( $matches[0] );
+                $tagEnd = strpos( $txt, '>', $tags['openWithTag'][ $id ] );
+                $openTag = substr( $txt, $tags['openWithTag'][ $id ], ( $tagEnd - $tags['openWithTag'][ $id ] + 1) );
                 
-                // Loop through all of our matches
-                for($i = 0; $i < $codeboxCount; $i++)
-                {
-                    $options = array();
+                $origLength = $tags['closeWithTag'][ $id ] - $tags['openWithTag'][ $id ];
+                
+                
+                $lang = 'php';
+                $line = 0;
+                
+                if( preg_match( '#_lang-(\w+)#i', $openTag, $matches ) )
+                    $lang = $matches[1];
                     
-                    // Grab the other classes (options in this case) 
-                    list($options['lang'], $options['lineNum'] ) = explode(' ', trim( $matches[2][$i] . $matches[3][$i] ) );
-                    $options['lang'] = str_replace( '_lang-', '', $options['lang'] );
-                    $options['lineNum'] = str_replace( '_linenums:', '', $options['lineNum'] );
-                    
-                    // Replace the current with the new
-                    $replacement = $this->_colorfy( $matches[4][$i], $options );
-                    $txt = str_replace( $matches[0][$i] , $replacement, $txt );
-                }
+                if( preg_match( '#_linenums:(\d+)#i', $openTag, $matches ) )
+                    $line = $matches[1];
+                
+                $content = html_entity_decode( substr( $txt, $tags['open'][ $id ], ( $tags['close'][ $id ] - $tags['open'][ $id ] ) ) );
+                $content = $this->_colorfy( $content, array( 'lang' => $lang, 'lineNum' => $line ) );
+                
+                $newLength = strlen( $content );
+                
+                $txt = substr_replace( $txt, $content, $tags['openWithTag'][ $id ], ( $tags['closeWithTag'][ $id ] - $tags['openWithTag'][ $id ] ) );
+                
+                // Update all lengths (we're cheating here!)
+                $tags = $this->_parent->getTagPositions( $txt, 'pre', array( '<' , '>' ) );
             }
         }
         
         // Run the existing for bbcode versions
-		parent::_replaceText( $txt );
+        parent::_replaceText( $txt );
         
         // Return the awesome looking code
-		return $txt;
-	}
+        return $txt;
+    }
     
-	/**
-	 * {@inherit}
-	 */
-	protected function _buildOutput( $content, $option )
-	{
-        // This is the original, used for BBCode.. which could cause issues with HTML (e.g. <br> in source)
+    /**
+     * {@inherit}
+     */
+    protected function _buildOutput( $content, $option )
+    {
+        // This is the original, used for BBCode ([code])
+        $content        = trim( $content );
         
-		$content        = trim( $content );
-		
-		$content = preg_replace( '#(<br(?:[^>]+?)?>)#i', '', $content );
-		$content = trim( $content );
-		
-		$content = str_replace( '<!-preserve.newline-->', "\n", $content );
+        $content = preg_replace( '#(<br(?:[^>]+?)?>)#i', '', $content );
+        $content = trim( $content );
+        
+        $content = str_replace( '<!-preserve.newline-->', "\n", $content );
         
         // Grab the option
         $lineNums = 1;
-		$langAdd  = '';
-		
-		if ( !is_array( $option ) )
-		{
-            list( $option['lang'], $option['lineNum']) = explode( ':', $option );
-		}
-		
+        $langAdd  = '';
+        
+        if ( ! is_array( $option ) )
+        {
+            list( $options['lang'], $options['lineNum']) = explode( ':', $option );
+        }
+        else
+        {
+            $options = $option;
+        }
+        
         // Make it pretty!
-        return $this->__colorfy( $content, $option );
-	}
+        return $this->_colorfy( $content, $options );
+    }
     
 
 	/**
